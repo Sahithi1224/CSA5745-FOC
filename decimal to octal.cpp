7dcad29ed09e1355adb33403974e4106c926a637
Sahithi1224 <112922087+Sahithi1224@users.noreply.github.com>
2023-02-03 19:41:30 +0530

    Add files via upload

---
+++ decimal to octal.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	int number;
+	printf("\nenter the deciaml number :");
+	scanf("%d",&number);
+	printf("\nthe octal number of a given number=%o",number);
+	return 0;
+}
